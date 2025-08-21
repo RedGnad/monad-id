613c5a934b6cdc451ebf164745c3026e7dcefcfe
Red.G <red.g@mbp-de-redg.home>
2025-08-21 23:47:37 +0200

    Fix Privy App ID configuration

---
+++ monad-react-webview/assets/cursor-BqHCtZg6.js
@@ -0,0 +1,4 @@
+import{m as o}from"./core-EwBZpuyi.js";import"./index-D-AsEuMl.js";const i=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{i as cursorSvg};
+//# sourceMappingURL=cursor-BqHCtZg6.js.map
