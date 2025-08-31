be87ca4f104f66857455e01bc97d845e8f1fa114
Red.G <red.g@mbp-de-redg.home>
2025-08-31 20:45:37 +0200

    Update monad-react-webview

---
+++ monad-react-webview/assets/cursor-D0Ko3i8T.js
@@ -0,0 +1,4 @@
+import{m as o}from"./core-BEObtTra.js";import"./index-gDiq5l8I.js";const i=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{i as cursorSvg};
+//# sourceMappingURL=cursor-D0Ko3i8T.js.map
