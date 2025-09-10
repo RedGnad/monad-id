c4f1acde9f2c0bdbf4b6f4147d026429f923bcfb
Red.G <red.g@mbp-de-redg.home>
2025-09-10 23:18:06 +0200

    .

---
+++ assets/close-Bu4-mF79.js
@@ -0,0 +1,9 @@
+import{m as l}from"./core-B5bPM2vo.js";import"./index-BqY1q9Wg.js";const r=l`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M2.54 2.54a1 1 0 0 1 1.42 0L8 6.6l4.04-4.05a1 1 0 1 1 1.42 1.42L9.4 8l4.05 4.04a1 1 0 0 1-1.42 1.42L8 9.4l-4.04 4.05a1 1 0 0 1-1.42-1.42L6.6 8 2.54 3.96a1 1 0 0 1 0-1.42Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{r as closeSvg};
+//# sourceMappingURL=close-Bu4-mF79.js.map
